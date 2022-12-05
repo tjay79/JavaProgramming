0fbd2ac9feb37e63a80de8e0cbcdf0dc5142a7f2
Anthony Arabia <tjay15787@gmail.com>
2022-12-05 12:48:01 +0100

    test adding more work

--- Java Practice/src/day36_PracticeTasks/ScrumTask/ProductOwner.java
+++ Java Practice/src/day36_PracticeTasks/ScrumTask/ProductOwner.java
@@ -1,9 +1,9 @@
 package day36_PracticeTasks.ScrumTask;
 
 public class ProductOwner extends Employee{
-    public ProductOwner(String name, int age, char gender, String jobTitle, int id, double salary, String companyName) {
-        super(name, age, gender, jobTitle, id, salary, companyName);
+
+
+    public ProductOwner(String name, int age, char gender, int id, double salary, String companyName) {
+        super(name, age, gender, "Product owner", id, salary, companyName);
     }
-
-
 }
